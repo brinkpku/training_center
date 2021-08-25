eac5401f5ba6cb990e323e91ead97d3725045e6a
youmengyuan <youmengyuan@sensetime.com>
2021-08-25 19:43:34 +0800

    date

--- date/main.go
+++ date/main.go
@@ -6,8 +6,16 @@ import (
 )
 
 func main() {
-	start := time.Unix(1613356602, 0)
+	start := time.Now()
 	for ; start.Before(time.Now()); start = start.Add(time.Hour * 24) {
 		fmt.Println(start.Format("sensego/plate/2006/01/02"))
 	}
+	today := getTodayZero()
+	fmt.Println(today.Add(-time.Hour * 24 * 90).Format("2006/01/02"))
+}
+
+func getTodayZero() time.Time {
+	now := time.Now()
+	zero := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
+	return zero
 }
