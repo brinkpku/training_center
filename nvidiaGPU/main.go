306739ba6a67d8eb259137f03c59d6852627d1e9
youmengyuan <youmengyuan@sensetime.com>
2021-10-25 02:49:11 +0000

    some update

--- nvidiaGPU/main.go
+++ nvidiaGPU/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/NVIDIA/gpu-monitoring-tools/bindings/go/nvml"
@@ -18,17 +19,18 @@ func init() {
 func main() {
 	nvml.Init()
 	defer nvml.Shutdown()
-	device, err := nvml.NewDevice(deviceID)
-	if err != nil {
-		fmt.Println("nvml new device error:", err)
-		return
-	}
-	attr, err := device.GetAttributes()
-	if err != nil {
-		fmt.Println("get attr error:", err)
-	}
-	fmt.Printf("device %v attr: %v\n", deviceID, attr)
 	for i := 0; i < 2; i++ {
+		deviceID = uint(i)
+		device, err := nvml.NewDevice(deviceID)
+		if err != nil {
+			fmt.Println("nvml new device error:", err)
+			return
+		}
+		attr, err := device.GetAttributes()
+		if err != nil {
+			fmt.Println("get attr error:", err)
+		}
+		fmt.Printf("device %v attr: %v\n", deviceID, attr)
 		status, err := device.Status()
 		if err != nil {
 			fmt.Println("get status error:", err)
@@ -37,6 +39,16 @@ func main() {
 		// fmt.Printf("device %v status: %v\n", deviceID, status)
 		fmt.Println("memory free", *status.Memory.Global.Free, "used", *status.Memory.Global.Used)
 		fmt.Println("utilization", *status.Utilization.Memory)
+		free, _ := strconv.ParseInt(fmt.Sprintf("%d", *status.Memory.Global.Free), 10, 64)
+		used, _ := strconv.ParseInt(fmt.Sprintf("%d", *status.Memory.Global.Used), 10, 64)
+		fmt.Println("free", free)
+		fmt.Println("used", used)
+		total := free + used
+		if total == 0 {
+			fmt.Println("get toal memory 0")
+			return
+		}
+		fmt.Println(float64(used) / float64(total))
 		fmt.Println("temperature", *status.Temperature)
 		fmt.Println("power", *status.Power)
 		time.Sleep(time.Second * 5)
