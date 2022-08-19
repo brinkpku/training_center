df7a788d64b47a7ceef61c31c1b9fc6dba755ccf
youmengyuan <youmengyuan@sensetime.com>
2022-08-19 15:20:01 +0800

    add comments

--- panic/main.go
+++ panic/main.go
@@ -2,15 +2,15 @@ package main
 
 func main() {
 	defer func() {
-		println("after panic")
+		println("after panic") // will run even the second defer panic
 	}()
 	defer func() {
-		panic("panic in defer")
+		panic("panic in defer") // will run even the last defer panic
 	}()
 	defer func() {
 		if e := recover(); e != nil {
 			println("recover from", e.(string))
-			panic("error again")
+			panic("error again") // run with LIFO order
 		}
 	}()
 	panic("unexpected error")
