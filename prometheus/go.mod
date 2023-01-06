d4cdbed5939fd0a9cb8179f0b80101c18a56db6d
youmengyuan <youmengyuan@sensetime.com>
2023-01-06 16:36:49 +0800

    add prometheus golang cli test

---
+++ prometheus/go.mod
@@ -0,0 +1,8 @@
+module github.com/brinkpku/training_center/prometheus
+
+go 1.17
+
+require (
+	github.com/prometheus/client_golang v1.13.1
+	github.com/prometheus/client_model v0.3.0 // indirect
+)
---
+++ prometheus/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func recordMetrics() {
+	go func() {
+		for {
+			opsProcessed.Inc()
+			processDur.Observe(float64(rand.Intn(15)))
+			actionCounter.WithLabelValues([2]string{"post", "get"}[rand.Intn(2)]).Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+	processDur = promauto.NewHistogram(prometheus.HistogramOpts{
+		Name: "myapp_processed_duration",
+		Help: "The latency of process",
+	})
+	actionCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "myapp_action",
+		Help: "The action num",
+	}, []string{"type"})
+)
+
+func main() {
+	recordMetrics()
+
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(opsProcessed, processDur, actionCounter)
+	hanlder := promhttp.HandlerFor(registry, promhttp.HandlerOpts{
+		Registry: registry,
+	})
+	// default handler
+	// http.Handle("/metrics", promhttp.Handler())
+	http.Handle("/metrics", hanlder)
+	http.ListenAndServe(":2112", nil)
+}
